9fc53c77f03363a6583398c9a581ffeec6f8e015
Joel-Cooper <161271563+Joel-Cooper@users.noreply.github.com>
2025-02-05 15:35:37 +1100

    Update doSlides.js

--- assets/js/doSlides.js
+++ assets/js/doSlides.js
@@ -12,81 +12,66 @@
     ================================================ */
 
               
-    function doSlides(images, containerSelector, delay=3000) {
-
+    function doSlides(images, containerSelector, delay = 3000) {
         // Elements
         let container = document.querySelector(containerSelector);
-
         let img = container.querySelector('img');
-
         let prefetch = new Image();
-        
-
-
+    
         // Add Button
-        let button = document.createElement('button');
-        button.id = 'run-button';       
-        button.textContent = 'Start';
-        container.insertAdjacentElement('beforeend', button);
-
-        
+        // let button = document.createElement('button');
+        // button.id = 'run-button';
+        // button.textContent = 'Start';
+        // container.insertAdjacentElement('beforeend', button);
+    
         // Add Caption             
         let caption = document.createElement('p');  
         container.insertAdjacentElement('beforeend', caption);
-         
-
+    
         // Variables
         let slideNumber = 0;
-        let running;    // undefined
-        
-        //img.addEventListener('click', toggle);
-        button.addEventListener('click', toggle);
-
+        let running; // undefined
+    
+        // button.addEventListener('click', toggle);
         next();
-
-
-        document.onkeypress = event => {
-            if(event.key == ' ') {
+    
+        document.addEventListener("keypress", event => {
+            if (event.code === "Space") {
                 toggle();
                 event.preventDefault();
-            }                
-
-                
-        };
+            }
+        });
     
-
-
         function toggle() {
-            if(!running){
+            if (!running) {
                 running = setInterval(next, delay);
                 next(); 
-                button.textContent = 'Stop';
+                // button.textContent = 'Stop';
                 container.classList.add('running');
-            }
-            else{
-                running = clearInterval(running);
-                button.textContent = 'Start';
+            } else {
+                clearInterval(running);
+                running = undefined;
+                // button.textContent = 'Start';
                 container.classList.remove('running');
             }
         }
-
-
-        function next(){
-            //Populate Image
-            //img.src = `images/slides/${images[slideNumber++]}`;
+    
+        function next() {
+            // Populate Image
             img.src = `images/project_1/${images[slideNumber].src}`;
             caption.textContent = images[slideNumber].caption;
             img.title = images[slideNumber].caption;
             img.alt = images[slideNumber].caption;
             slideNumber++;
-            
+    
             // Wrap around
             if (slideNumber >= images.length) slideNumber = 0;
-
-            // Predetch
-            //prefetch.src = `images/slides/${images[slideNumber]}`;
+    
+            // Prefetch
             prefetch.src = `images/project_1/${images[slideNumber].src}`;
         }
-
-              
+    
+        // Auto-start slideshow after a short delay
+        setTimeout(() => toggle(), 100);
     }
+    
